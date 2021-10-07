1e697b5dd46e23bd4d1bae30f06c2e957c585887
Benjamin Barbe <benjamin@pandasuite.com>
2021-10-07 12:58:44 +0200

    Shared schema component

--- src/index.js
+++ src/index.js
@@ -1,44 +1,148 @@
 import PandaBridge from 'pandasuite-bridge';
+import { io } from 'socket.io-client';
+import { create } from 'jsondiffpatch';
+
+import merge from 'lodash/merge';
+import compact from 'lodash/compact';
+import get from 'lodash/get';
+import set from 'lodash/set';
+import unset from 'lodash/unset';
+import pull from 'lodash/pull';
+import remove from 'lodash/remove';
+import isArray from 'lodash/isArray';
+
+import JSONPointer from '@beingenious/jsonpointer';
+
 import './index.css';
 
-let properties = null;
-let markers = null;
+const diffpatcher = create();
+const NUMERIC_DIFFERENCE = -8;
 
-function myInit() {
-  // const imageUrl = PandaBridge.resolvePath('my_image.png');
-  // PandaBridge.send('imageChanged');
-}
+const numericDiffFilter = (context) => {
+  if (typeof context.left === 'number' && typeof context.right === 'number' && context.right !== context.left) {
+    context.setResult([0, context.right - context.left, NUMERIC_DIFFERENCE]).exit();
+  }
+};
+numericDiffFilter.filterName = 'numeric';
+diffpatcher.processor.pipes.diff.before('trivial', numericDiffFilter);
+
+let properties = null;
+let socket = null;
+let schema = {};
+
+const initSocketIO = () => {
+  const { url, room } = properties || {};
+
+  if (!url) {
+    return;
+  }
+
+  socket = io(url, {
+    reconnection: true,
+    reconnectionDelay: 1000,
+    reconnectionDelayMax: 5000,
+    reconnectionAttempts: Infinity,
+    query: {
+      room,
+    },
+  });
+
+  socket.on('connect', () => {
+    PandaBridge.send('onSignedIn');
+  });
+
+  socket.on('disconnect', (reason) => {
+    PandaBridge.send('onSignedOut');
+
+    if (reason === 'io server disconnect') {
+      socket.connect();
+    }
+  });
+
+  socket.on('schema', (newSchema) => {
+    schema = newSchema;
+
+    PandaBridge.send(PandaBridge.UPDATED, {
+      queryable: schema,
+    });
+  });
+};
+
+const getPointer = (data, pointer) => {
+  let resolvedPointer = [];
+
+  const value = JSONPointer.resolvePointer(
+    data,
+    JSONPointer.getPointerByJSONPointer(pointer),
+    {
+      unitPool: {
+        language: navigator.language.replace('-', '_'),
+      },
+    }, undefined, undefined, resolvedPointer,
+  );
+
+  if (!value) {
+    resolvedPointer = compact(pointer.replace(/@[^:]+:/g, '').split('/'));
+  }
+  return resolvedPointer;
+};
 
 PandaBridge.init(() => {
   PandaBridge.onLoad((pandaData) => {
     properties = pandaData.properties;
-    markers = pandaData.markers;
 
     if (document.readyState === 'complete') {
-      myInit();
+      initSocketIO();
     } else {
-      document.addEventListener('DOMContentLoaded', myInit, false);
+      document.addEventListener('DOMContentLoaded', initSocketIO, false);
     }
   });
 
   PandaBridge.onUpdate((pandaData) => {
     properties = pandaData.properties;
-    markers = pandaData.markers;
-  });
-
-  /* Markers */
-
-  PandaBridge.getSnapshotData(() => null);
-
-  PandaBridge.setSnapshotData((pandaData) => {
-    // pandaData.data.id
   });
 
   /* Actions */
 
-  PandaBridge.listen('changeColor', (args) => {
-  });
+  PandaBridge.listen('change', ([{ data: key, function: func, value }]) => {
+    const pointer = getPointer(schema, key);
+    const newSchema = merge({}, schema);
 
-  PandaBridge.synchronize('synchroImages', (percent) => {
+    const existingValue = get(schema, pointer);
+
+    if (func === 'set') {
+      set(newSchema, pointer, value);
+    } else if (func === 'inc') {
+      set(newSchema, pointer, (parseFloat(existingValue) || 0) + parseInt(value));
+    } else if (func === 'dec') {
+      set(newSchema, pointer, (parseFloat(existingValue) || 0) - parseInt(value));
+    } else if (func === 'del') {
+      unset(newSchema, pointer);
+    } else if (func === 'add') {
+      const existingArray = get(newSchema, pointer);
+
+      if (!isArray(existingArray)) {
+        set(newSchema, pointer, [value]);
+      } else {
+        existingArray.push(value);
+      }
+    } else if (func === 'delbyid') {
+      const existingArray = get(newSchema, pointer);
+
+      if (isArray(existingArray)) {
+        remove(existingArray, (row) => row.id === value);
+      }
+    } else if (func === 'delbyvalue') {
+      const existingArray = get(newSchema, pointer);
+
+      if (isArray(existingArray)) {
+        pull(existingArray, value);
+      }
+    }
+
+    const patch = diffpatcher.diff(schema, newSchema);
+    if (patch) {
+      socket.emit('schema', patch);
+    }
   });
 });
