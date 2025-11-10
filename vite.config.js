1dfd5befae3fe48348593efe486bf7681e4ed540
Benjamin Barbe <benjamin@pandasuite.com>
2025-11-10 16:35:03 +0100

    Add @vitejs/plugin-legacy for improved browser compatibility

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite';
+import legacy from '@vitejs/plugin-legacy';
 
 export default defineConfig({
   base: './',
+  plugins: [
+    legacy({
+      targets: ['defaults', 'iOS >= 9'],
+      renderLegacyChunks: true,
+      modernPolyfills: true,
+    }),
+  ],
   server: {
     host: '0.0.0.0',
     port: 8080,
