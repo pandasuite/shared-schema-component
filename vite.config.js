9fe66f7944bb8cb05b5dbc74b77000d1ed27aca6
Benjamin Barbe <benjamin@pandasuite.com>
2026-01-29 16:29:41 +0100

    Remove package-lock.json, update package.json to use Tailwind CSS version 3, add caniuse-lite dependency, and adjust browserslist configuration. Update vite.config.js to target Chrome 87 and remove legacy plugin configuration. Update yarn.lock to reflect these changes.

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,24 @@
 import { defineConfig } from 'vite';
-import legacy from '@vitejs/plugin-legacy';
 
 export default defineConfig({
   base: './',
-  plugins: [
-    legacy({
-      targets: ['defaults', 'iOS >= 9'],
-      renderLegacyChunks: true,
-      modernPolyfills: true,
-    }),
-  ],
   server: {
     host: '0.0.0.0',
     port: 8080,
   },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'chrome87',
+    },
+  },
+  esbuild: {
+    target: 'chrome87',
+  },
   build: {
     outDir: 'build',
     emptyOutDir: true,
+    target: 'chrome87',
+    minify: 'esbuild',
   },
   define: {
     'process.platform': '"web"',
